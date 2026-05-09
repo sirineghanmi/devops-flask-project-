7336418b9d7c66bba082e74259bae6eaabcf5334
Sirine Ghanmi <sirineg041@gmail.com>
2026-05-09 16:23:58 +0100

    fix: restore routes with USER_BLUEPRINT

--- src/routes/user.py
+++ src/routes/user.py
@@ -1,44 +1,12 @@
-from flasgger import Swagger
-from flask import Flask, Blueprint
+"""
+Defines the blueprint for the users
+"""
+from flask import Blueprint
+from flask_restful import Api
 
-from src import config
-from src.models import db
+from src.resources import UserResource
 
-server = Flask(__name__)
-app = server
-
-# ---------------- Swagger config ----------------
-server.config["SWAGGER"] = {
-    "swagger_version": "2.0",
-    "title": "Application",
-    "specs": [
-        {
-            "version": "0.0.1",
-            "title": "Application",
-            "endpoint": "spec",
-            "route": "/application/spec",
-            "rule_filter": lambda rule: True,
-        }
-    ],
-    "static_url_path": "/apidocs",
-}
-
-Swagger(server)
-
-# ---------------- DB config ----------------
-server.debug = config.DEBUG
-server.config["SQLALCHEMY_DATABASE_URI"] = config.DB_URI
-server.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = config.SQLALCHEMY_TRACK_MODIFICATIONS
-
-db.init_app(server)
-
-# ---------------- IMPORT ROUTES (IMPORTANT: après création app) ----------------
-from src import routes
-
-for blueprint in vars(routes).values():
-    if isinstance(blueprint, Blueprint):
-        server.register_blueprint(blueprint, url_prefix=config.APPLICATION_ROOT)
-
-# ---------------- RUN ----------------
-if __name__ == "__main__":
-    server.run(host=config.HOST, port=config.PORT)
+USER_BLUEPRINT = Blueprint("user", __name__)
+Api(USER_BLUEPRINT).add_resource(
+    UserResource, "/user/<string:last_name>/<string:first_name>"
+)
